7427862b7a7f8d5562f76679007d1a72be8a0bd8
Tanja Süß <ajnat2268@gmail.com>
2022-01-18 15:30:17 +0100

    Created first storybook-buttons

---
+++ styles/components/button.stories.js
@@ -0,0 +1,8 @@
+import "./button.css";
+
+export default {
+  title: "Component/Button",
+};
+
+export const Regular = () => `<button class="button">Click me!</button>`;
+export const Colorchange = () => `<button class="button--colorchange">Click me!</button>`;
