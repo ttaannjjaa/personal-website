9948fdca16199e20a2a5e50a6acac49185edea3e
Tanja Süß <ajnat2268@gmail.com>
2022-01-18 23:23:46 +0100

    Created second storybook component

--- styles/components/giticon/giticon.stories.js
+++ styles/components/giticon/giticon.stories.js
@@ -1,9 +1,14 @@
 import "./giticon.css";
-import ".GitHub-Mark-64px.png";
+import imageFile from "./GitHub-Mark-64px.png";
 import html from "./giticon.html";
 
 export default {
   title: "Component/Giticon",
+};
+
+const image = {
+  src: imageFile,
+  alt: "GitHubLogo",
 };
 
 export const All = () => html;
---
+++ styles/components/nav-bar/nav-bar.stories.js
@@ -0,0 +1,9 @@
+import "./nav-bar.css";
+
+import html from "./nav-bar.html";
+
+export default {
+  title: "Component/Nav-bar",
+};
+
+export const All = () => html;
