9a1fd1ff04771a1b242639bb1ddddcb7a1a7a09e
Tanja Süß <ajnat2268@gmail.com>
2022-01-18 12:04:34 +0100

    Installed storybook and configuration

---
+++ .storybook/preview.js
@@ -0,0 +1,13 @@
+import "../styles/base.css";
+import "../styles/utility.css";
+import "../styles/colors.css";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
